e01d3c8a045d7dba55f492304853933d08414909
plein <68096727+planetoryd@users.noreply.github.com>
2025-07-13 06:44:26 +0800

    dev

--- strip.sh
+++ strip.sh
@@ -2,3 +2,6 @@
 strip ./target/release/sproxy
 # dont strip /nsproxy
 # otherwise no backtrace. sproxy is trivial
+mkdir release
+cp ./target/release/sproxy ./release
+cp ./target/release/nsproxy ./release
