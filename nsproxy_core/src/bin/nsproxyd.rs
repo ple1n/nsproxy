2541a86353b806a7d699cdec72c931e31a2052bc
plein <68096727+planetoryd@users.noreply.github.com>
2025-09-02 15:27:42 +0800

    toml. builds

--- nsproxy_core/src/bin/nsproxyd.rs
+++ nsproxy_core/src/bin/nsproxyd.rs
@@ -4,6 +4,8 @@ use clap::{
     Parser, Subcommand, ValueEnum,
     builder::{TypedValueParser, ValueParser, ValueParserFactory},
 };
+use nix::unistd::Pid;
+use nsproxy_common::{ExactNS, NSFrom};
 use serde::{Deserialize, Serialize};
 use std::{
     fs,
@@ -17,7 +19,7 @@ use tun2socks5::ArgMode;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Working state directory. defaults to "./"
+    /// Working state directory
     #[arg(short, long, default_value = "./")]
     output: PathBuf,
     /// Source network namespace (src=/path OR src=1234)
@@ -32,33 +34,25 @@ struct Cli {
     /// Make veths (optional name, default "veth0")
     #[arg(long, value_name = "NAME", default_missing_value = "veth0")]
     veth: Option<Option<String>>,
-
     /// change uid after entering shell
     #[arg(short, long)]
     uid: Option<u32>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Parser)]
-pub struct NsArg {
-    #[arg(long)]
-    path: Option<PathBuf>,
-
-    #[arg(long)]
-    pid: Option<i32>,
-}
-
 /// Representation of a namespace input (PID or Path)
 #[derive(Debug, Serialize, Deserialize, Clone)]
 enum NsInput {
-    Pid(u32),
+    Pid(i32),
     Path(PathBuf),
+    /// this process
+    This,
 }
 
 impl std::str::FromStr for NsInput {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Ok(pid) = s.parse::<u32>() {
+        if let Ok(pid) = s.parse::<i32>() {
             Ok(NsInput::Pid(pid))
         } else {
             Ok(NsInput::Path(PathBuf::from(s)))
@@ -68,11 +62,30 @@ impl std::str::FromStr for NsInput {
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    dbg!(&cli.veth);
-    // Ensure ./nsproxy.state exists
     let state_dir = PathBuf::from("./nsproxy.state");
     fs::create_dir_all(&state_dir)?;
-
-    println!("State written to {:?}", cli.output);
+    let pid = nix::unistd::Pid::this();
+    let src: ExactNS = NSFrom::from_source(cli.src.resolve(pid))?;
+    let dst: ExactNS = NSFrom::from_source(cli.dst.resolve(pid))?;
+    
     Ok(())
+}
+
+impl NsInput {
+    pub fn resolve(self, this_pid: Pid) -> Self {
+        match &self {
+            NsInput::Pid(_) | NsInput::Path(_) => self,
+            NsInput::This => NsInput::Pid(this_pid.as_raw()),
+        }
+    }
+}
+
+impl NSFrom<NsInput> for ExactNS {
+    fn from_source(source: NsInput) -> anyhow::Result<Self> {
+        match source {
+            NsInput::Path(p) => NSFrom::from_source(p),
+            NsInput::Pid(p) => NSFrom::from_source(p),
+            NsInput::This => unreachable!()
+        }
+    }
 }
