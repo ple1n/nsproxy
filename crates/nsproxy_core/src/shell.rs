e8298330d26be6458a5c2a725fae4519352da936
plein <68096727+planetoryd@users.noreply.github.com>
2025-09-12 22:17:39 +0800

    gids

--- crates/nsproxy_core/src/shell.rs
+++ crates/nsproxy_core/src/shell.rs
@@ -1,15 +1,15 @@
 use std::{
     env::{current_dir, var},
     ffi::{CStr, CString, OsString},
-    os::unix::{process::CommandExt, raw::uid_t},
+    os::unix::{process::CommandExt, raw::{gid_t, uid_t}},
     path::PathBuf,
     str::FromStr,
 };
 
 use anyhow::{anyhow, bail};
-use nix::unistd::{execve, getresuid, setresuid};
+use nix::unistd::{Gid, execve, getegid, getresuid, setgroups, setresgid, setresuid};
 use nsproxy_common::UID_HINT_VAR;
-use tracing::warn;
+use tracing::{info, warn};
 use uzers::{Group, os::unix::UserExt};
 
 use crate::{
@@ -25,6 +25,8 @@ use crate::{
 pub struct ShellPrefs {
     gids: Vec<Group>,
     pub uid: Option<uid_t>,
+    /// Main GID
+    pub gid: Option<gid_t>,
     /// This can also just be a program despite the struct's name
     pub shell: Option<PathBuf>,
     /// The user wants to drop in a root shell
@@ -68,6 +70,18 @@ impl ShellPrefs {
             {
                 self.gids = gids;
             }
+            if self.gid.is_none() {
+                self.gid = Some(gid_in);
+            } else {
+                let egid = getegid();
+                if egid.as_raw() != 0 {
+                    self.gid = Some(egid.as_raw())
+                } else {
+                    if !self.wants_root {
+                        bail!("can not decide gid to use");
+                    }
+                }
+            }
             let ushell = user.shell().to_owned();
             self.shell = ushell.into();
         }
@@ -99,16 +113,15 @@ impl ShellPrefs {
             match &clone {
                 Clone3Result::IsChild { tx } => {
                     let cmd = CString::new(cmd.to_str().unwrap())?;
-                    execve(cmd.as_c_str(), &self.args, &self.env);
+                    self.drop_privs()?;
 
+                    execve(cmd.as_c_str(), &self.args, &self.env);
                 }
                 Clone3Result::Parent {
                     child_pid,
                     child_pidfd,
                     tx,
-                } => {
-
-                },
+                } => {}
             }
             Ok(clone)
         } else {
@@ -139,10 +152,21 @@ impl ShellPrefs {
         aok!()
     }
     pub fn drop_privs(&self) -> Result<()> {
+        info!("drop privs, gids to {:?}", self.gids_raw());
+        setgroups(
+            &self
+                .gids_raw()
+                .iter()
+                .map(|g| Gid::from_raw(*g))
+                .collect::<Vec<_>>(),
+        )?;
+        let g = self.gid.unwrap().into();
+        setresgid(g, g, g);
         if let Some(uid) = self.uid {
             let u = uid.into();
             setresuid(u, u, u)?;
         }
+
         Ok(())
     }
 }
