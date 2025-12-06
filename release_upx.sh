e00a64c40ddd552aaaa1257ff2defeca7fc63b02
plein <68096727+planetoryd@users.noreply.github.com>
2025-12-06 22:30:03 +0800

    3.2.3

--- release_upx.sh
+++ release_upx.sh
@@ -2,9 +2,11 @@ mkdir -p release
 cargo b -r --bins -p nsproxy_core
 cp ./target/release/sproxy ./release
 cp ./target/release/nsproxy ./release
+cp ./target/release/nswrap ./release
 cp ./release/nsproxy ./release/nsproxy_debug
 strip ./release/sproxy
 strip ./release/nsproxy
+strip ./release/nswrap
 upx ./release/sproxy
 upx ./release/nsproxy
 ls -lh release
