2021674990413ed9e4623512c4f54e89afb456ce
plein <68096727+planetoryd@users.noreply.github.com>
2026-04-29 17:21:07 +0800

    script

--- push_all.fish
+++ push_all.fish
@@ -4,9 +4,11 @@
 for submodule in (git submodule status --recursive | awk '{ print $2 }')
     echo "Pushing submodule: $submodule"
     # git submodule update --remote --merge $submodule
-    git -C $submodule push
+    git -C "$submodule" push
+    or exit $status
 end
 
 # Finally, push the main repository itself
 echo "Pushing main repository"
 git push
+or exit $status
